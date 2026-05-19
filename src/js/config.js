cdae0d92db3f8cd70c83cb9098a9a1965a86128b
sahilsahibzada4 <sahilsahibzada4@gmail.com>
2026-05-19 20:42:58 +0430

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8891755a-1ea0-4735-a39d-cb1a8b7bbe2a';
+export const MODAL_CLOSE_SEC = 2.5;
